edac1d3302d6fbef67d102af8c6e0be6d0b77bb9
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-24 00:27:33 +0900

    Time: 8207 ms (13.9%), Space: 12.5 MB (52.34%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -9,12 +9,16 @@ class Solution(object):
         traserving = set()
 
         def dfs(row, col, idx):
+            # 기저조건 1: word에서 찾을 단어를 모두 찾은 경우 TRUE 반환
             if idx == len(word):
                 return True
+            # 기저조건 2: 찾고있는 위치가 범위를 벗어나면 FALSE 반환
             if not (0<= row < n_rows and 0 <= col < n_cols):
                 return False
+            # 기저조건 3: 찾는 숫자가 없으면 FALSE 반환
             if board[row][col] != word[idx]:
                 return False
+            # 기저조건 4: 이미 방문했던 경로인 경우 FALSE 반환
             if (row, col) in traserving:
                 return False
             

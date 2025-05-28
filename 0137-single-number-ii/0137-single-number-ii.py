629ae2dd8a27ec575cf02a30abcfcef46e81384b
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-28 16:30:19 +0900

    Time: 0 ms (100%), Space: 13.3 MB (86.94%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return (sum(set(nums))*3 - sum(nums)) / 2
+        # return (sum(set(nums))*3 - sum(nums)) / 2
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones

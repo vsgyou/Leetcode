66ff74ef10ec2750bcd5912be6854ddf9d795ebb
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-29 23:02:55 +0900

    Time: 0 ms (100%), Space: 12.6 MB (14.52%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        nums = [ 1, 2, 3, 4]        
+        
+        - 인덱스 0에 있는 집을 털었을 때 최대 금액 : nums[0] + F(nums[2:])
+        - 인덱스 0에 있는 집을 안 털었을 때 최대 금액 : F[nums[1:]]
+
+        F[nums] = MAX(nums[0] + F(nums[2:]), F[nums[1:]])
+        F[start] = MAX(nums[start]) + F(start + 2), F(start + 1))
+        """
+        # def dfs(start):
+        #     if start >= len(nums):
+        #         return 0
+        #     return max(nums[start] + dfs(start+2), dfs(start + 1))
+        
+        # return dfs(0)
+
+        # 시간 복잡도 개선
+
+        memo = {}
+        
+        def dfs(start):
+            if start in memo:
+                return memo[start]
+
+            if start >= len(nums):
+                memo[start] = 0
+            else:
+                memo[start] = max(nums[start] + dfs(start+2), dfs(start+1))
+            return memo[start]
+        
+        return dfs(0)
+            
+
+

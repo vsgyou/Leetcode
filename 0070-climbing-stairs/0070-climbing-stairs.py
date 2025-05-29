4cd4a4e8408795d07c4b647618732e89c4a228ca
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-29 22:27:18 +0900

    Time: 0 ms (100%), Space: 12.3 MB (98.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        
+           -
+          -
+         -
+        -
+        n=1, 1가지
+        n=2, 2가지 (1,1), (2)
+        n=3, 1에 도착한 경우 + 2에 도착한 경우 = 3가지
+        n=4, 2에 도착한 경우 + 3에 도착한 경우 = 5가지
+        .
+        .
+        .
+        f(n) = f(n-2) + f(n-1)
+        """
+
+        dp = {1:1, 2:2}
+        for i in range(3, n+1):
+            dp[i] = dp[i-2] + dp[i-1]
+        
+        return dp[n]
+        
+        

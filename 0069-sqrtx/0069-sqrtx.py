5be316b14d8356d4640abfc547eeea77eb022ba4
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-29 17:27:53 +0900

    Time: 36 ms (16.05%), Space: 12.4 MB (81.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -4,5 +4,32 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        sq_x = sqrt(x)
-        return int(sq_x)
+        # # 이진탐색 방법
+        # if x == 0:
+        #     return 0
+        
+        # left, right = 0, x
+
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if mid * mid == x:
+        #         return mid
+        #     elif mid * mid < x:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        # return right
+
+        # 뉴턴의 방법
+        if x == 0 :
+            return 0
+        
+        guess = x
+        while guess * guess > x:
+            guess = (guess + x // guess) // 2
+            print(guess)
+        return guess
+        
+        """
+        x = 4, guess = 4, guess = (4 + 4 // 4) // 4 = (2) // 2 = 0
+        """

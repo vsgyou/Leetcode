a9908467175c37eaaf4f65da2a695aaae167d7b2
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-29 16:09:03 +0900

    Time: 0 ms (100%), Space: 12.4 MB (50.72%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digits[-1] = digits[-1]+1
-        digits = [0] + digits
-        next_num = 0 
-        result = []
-        while digits:
-            digit = digits.pop()
-            digit = next_num + digit
-            next_num = digit // 10
-            current_num = digit % 10
+        # digits[-1] = digits[-1]+1
+        # digits = [0] + digits
+        # next_num = 0 
+        # result = []
+        # while digits:
+        #     digit = digits.pop()
+        #     digit = next_num + digit
+        #     next_num = digit // 10
+        #     current_num = digit % 10
 
-            result.append(current_num)
-        result = result[::-1]
-        if result[0] == 0:
-            result = result[1:]
-        return result
+        #     result.append(current_num)
+        # result = result[::-1]
+        # if result[0] == 0:
+        #     result = result[1:]
+        # return result
+
+        num = int("".join(map(str, digits))) + 1
+        return list(map(int, str(num)))

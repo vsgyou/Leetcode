545773fe503f6ce457cdcede0d38d0e561616366
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-30 02:31:35 +0900

    Time: 2185 ms (46.54%), Space: 12.7 MB (60.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -6,10 +6,10 @@ class Solution(object):
         """
         """
         [10, 9, 2, 5, 3, 7, 101, 18]
-
+        
         """
         dp = [1] * len(nums)
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)

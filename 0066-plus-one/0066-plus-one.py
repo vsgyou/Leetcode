29091fd2469dc3b12a6f155fbb5d2f26fce0ef99
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-29 16:06:25 +0900

    Time: 0 ms (100%), Space: 12.3 MB (79.64%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        digits[-1] = digits[-1]+1
+        digits = [0] + digits
+        next_num = 0 
+        result = []
+        while digits:
+            digit = digits.pop()
+            digit = next_num + digit
+            next_num = digit // 10
+            current_num = digit % 10
+
+            result.append(current_num)
+        result = result[::-1]
+        if result[0] == 0:
+            result = result[1:]
+        return result

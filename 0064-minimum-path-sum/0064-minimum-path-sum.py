9dd0a588853aeae85585394c5fbf6167a83770a9
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-30 14:48:31 +0900

    Time: 19 ms (42.28%), Space: 13.8 MB (81.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -12,18 +12,17 @@ class Solution(object):
                     2 - 1 = 10
                 4 - 2 - 1 = 9
         """
-        if not grid or not grid[0]:
+        if not grid and not grid[0]:
             return 0
+        
         m,n = len(grid), len(grid[0])
 
-        for i in range(1 ,m):
-            grid[i][0] += grid[i-1][0]
-        for j in range(1, n):
-            grid[0][j] += grid[0][j-1]
-
-        for i in range(1, m):
-            for j in range(1, n):
-                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-                
+        for row in range(1, m):
+            grid[row][0] = grid[row-1][0] + grid[row][0]
+        for col in range(1, n):
+            grid[0][col] = grid[0][col-1] + grid[0][col]
+        
+        for row in range(1, m):
+            for col in range(1, n):
+                grid[row][col] = grid[row][col]+min(grid[row-1][col], grid[row][col-1])
         return grid[-1][-1]
-                

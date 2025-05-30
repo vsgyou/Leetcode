9d2589d0cef3d2f23c50ccc5a64cddd58793f5f1
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-05-30 17:58:25 +0900

    Time: 38 ms (13.98%), Space: 12.6 MB (27.82%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -6,22 +6,23 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        N1, N2 = len(s1), len(s2)
-        if len(s3) != N1+N2:    return False
+        n1, n2 = len(s1), len(s2)
         
-        dp = [[False]*(N2+1) for _ in range(N1+1)]
+        if len(s3) != n1+n2:
+            return False
+        
+        dp = [[False]*(n2+1) for _ in range(n1+1)]
         dp[0][0] = True
-        
-        for n in range(1, N1+1):
+
+        for n in range(1, n1+1):
             dp[n][0] = (s1[n-1] == s3[n-1]) and dp[n-1][0]
         
-        for n in range(1, N2+1):
+        for n in range(1, n2+1):
             dp[0][n] = (s2[n-1] == s3[n-1]) and dp[0][n-1]
-        
-        for n in range(1, N1+1):
-            for m in range(1, N2+1):
+
+        for n in range(1, n1+1):
+            for m in range(1, n2+1):
                 t1 = (s1[n-1] == s3[n+m-1]) and dp[n-1][m]
-                t2 = (s2[m-1] == s3[n+m-1]) and dp[n][m-1]
+                t2 = (s2[m-1] == s3[n+m-1]) and dp [n][m-1]
                 dp[n][m] = t1 or t2
-                
-        return dp[N1][N2]
+        return dp[n1][n2]

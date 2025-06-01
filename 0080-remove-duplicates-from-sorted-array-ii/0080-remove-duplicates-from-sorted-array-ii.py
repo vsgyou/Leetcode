32a2988f550246970d86e5afafe3b0392ed6d236
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-01 16:36:50 +0900

    Time: 73 ms (44.9%), Space: 15.5 MB (16.27%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,18 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        num_count_dict = {}
-        count = 1
-        current = 0
-        n = len(nums)
-        while count <= n:
-            if nums[current] not in num_count_dict:
-                num_count_dict[nums[current]] = 1
-                current += 1
+        k = 0
+        count = 0
+
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i-1]:
+                count = 1
             else:
-                num_count_dict[nums[current]] += 1
-                if num_count_dict[nums[current]] >= 3:
-                    nums.pop(current)
-                else:
-                    current += 1
-            count += 1
+                count += 1
+            if count <= 2:
+                nums[k] = nums[i]
+                k += 1
+        return k

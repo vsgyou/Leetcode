174efa5a28a748c3e842010a3e2526a1461fc22f
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-02 15:30:56 +0900

    Time: 62 ms (92.75%), Space: 15.4 MB (56.63%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        k = 1
+        k = 0
+        curr = float("inf") 
         count = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
+        for i in range(len(nums)):
+            if nums[i] != curr:
+                curr = nums[i]
                 count = 1
-            else:
-                count += 1
             if count <= 2:
                 nums[k] = nums[i]
-                k+=1
+                count+=1
+                k += 1
         return k
+                

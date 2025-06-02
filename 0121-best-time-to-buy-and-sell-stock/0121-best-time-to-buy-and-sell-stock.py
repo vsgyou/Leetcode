e5a412e873965d941461d00539d792b25b36f1d1
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-02 16:08:06 +0900

    Time: 147 ms (11.24%), Space: 19 MB (90.18%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # 단순하게 반복문을 이용한 풀이. 시간복잡도는 O(n^2)
+        # max_profit = 0
+        # for i in range(len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         profit = prices[j] - prices[i]
+        #         max_profit = max(profit, max_profit)
+        # return max_profit
+        min_price = float("inf")
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit

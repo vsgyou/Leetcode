cfff60e76d59c559655223659a96c3e00b0d9df6
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-02 17:20:58 +0900

    Time: 11 ms (43.22%), Space: 13.1 MB (68.95%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -7,11 +7,17 @@ class Solution(object):
         jump_count = 0
         max_len = 0
         current_idx = 0
-
+        
         for i in range(len(nums)-1):
             max_len = max(max_len, nums[i] + i)
             if i == current_idx:
                 current_idx = max_len
                 jump_count += 1
+                if current_idx >= len(nums)-1:
+                    break
         return jump_count
-
+        """
+        [2,3,1,1,4]
+         ^   ^
+         i   max
+        """

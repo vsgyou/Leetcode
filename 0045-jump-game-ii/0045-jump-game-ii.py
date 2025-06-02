fdfad9fda9eab52fc8a94cdc0b28b9770dd1a5cf
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-02 17:27:23 +0900

    Time: 10 ms (48.27%), Space: 13.3 MB (45.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,20 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        jump_count = 0
-        max_len = 0
         current_idx = 0
+        max_idx = 0
+        jump_count = 0
         
         for i in range(len(nums)-1):
-            max_len = max(max_len, nums[i] + i)
+            max_idx = max(i+nums[i], max_idx)
             if i == current_idx:
-                current_idx = max_len
+                current_idx = max_idx
                 jump_count += 1
-                if current_idx >= len(nums)-1:
-                    break
+            if current_idx >= len(nums)-1:
+                break
         return jump_count
-        """
-        [2,3,1,1,4]
-         ^   ^
-         i   max
-        """

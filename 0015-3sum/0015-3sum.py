967bdae6dad14237a6b7321ae32645576fa1f5d5
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-04 23:26:38 +0900

    Time: 1946 ms (19.51%), Space: 20.5 MB (8.28%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,24 +5,41 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        result = []
-        for start in range(len(nums)-2):
-            if start >0 and nums[start] == nums[start-1]:
-                continue
-            left = start + 1
-            right = len(nums)-1
+        result = set()
+        for i in range(len(nums)-2):
+            left, right = i+1, len(nums)-1
             while left < right:
-                current_sum = nums[start] + nums[left] + nums[right]
-                if current_sum == 0 :
-                    result.append([nums[start],nums[left],nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left +=1 
-                    while left < right and nums[right] == nums[right -1]:
-                        right -=1
-                    left +=1 
-                    right -= 1
-                elif current_sum < 0:
+                hap = nums[i] + nums[left] + nums[right]
+                if hap < 0:
                     left += 1
-                else:
+                elif hap > 0:
                     right -= 1
-        return result
+                else:
+                    result.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+        return [list(num) for num in result]
+
+        # 이전코드 
+        # nums.sort()
+        # result = []
+        # for start in range(len(nums)-2):
+        #     if start >0 and nums[start] == nums[start-1]:
+        #         continue
+        #     left = start + 1
+        #     right = len(nums)-1
+        #     while left < right:
+        #         current_sum = nums[start] + nums[left] + nums[right]
+        #         if current_sum == 0 :
+        #             result.append([nums[start],nums[left],nums[right]])
+        #             while left < right and nums[left] == nums[left + 1]:
+        #                 left +=1 
+        #             while left < right and nums[right] == nums[right -1]:
+        #                 right -=1
+        #             left +=1 
+        #             right -= 1
+        #         elif current_sum < 0:
+        #             left += 1
+        #         else:
+        #             right -= 1
+        # return result

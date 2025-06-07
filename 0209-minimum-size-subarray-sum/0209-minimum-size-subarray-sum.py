e2bc7839f00620733827b55f5379b6c274016222
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-07 14:46:54 +0900

    Time: 14 ms (58.08%), Space: 20.2 MB (10.01%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,19 +1,49 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
         """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
+        nums = [2,3,1,2,4,3], target = 7
+        [2] = 2
+        [2,3] = 5
+        [2,3,1] = 6
+        [2,3,1,2] = 8
+        [2,3,1,2,4] = 필요 x
+        [2,3,1,2,4,3] = 필요 x
+        ------------- min_len = 4
+        [3] = 3
+        [3,1] = 4
+        [3,1,2] = 6
+        [3,1,2,4] = 10
+        [3,1,2,4,3] = 필요 x
+        ------------ min_len =4
+        [1] = 1
+        [1,2] = 3
+        [1,2,4] = 7
+        [1,2,4,3] = 필요 x
+        ------------ min_len = 3
         """
-        left = 0
-        current_sum = 0 
-        min_length = float("inf")
-        for right in range(len(nums)):
-            current_sum += nums[right]
-            while current_sum >= target:
-                min_length = min(min_length, right - left + 1)
-                current_sum -= nums[left]
-                left += 1
-        if min_length == float("inf"):
-            return 0
-        return min_length
+        # min_len = float("inf")
+        # for i in range(len(nums)):
+        #     tmp = []
+        #     for j in range(i, min(i+min_len,len(nums))):
+        #         tmp.append(nums[j])
+        #         print(tmp, sum(tmp))
+        #         if sum(tmp) >= target:
+        #             min_len = min(len(tmp), min_len)
+        #             break
+        # if min_len == float("inf"):
+        #     min_len = 0
+        # return min_len            
+
+        # 시간이 오래걸림 -> Sliding Window
+
+        start = 0
+        min_len = float("inf")
+        curr_sum = 0
+        for end in range(len(nums)):
+            curr_sum += nums[end]
+            while curr_sum >= target:
+                min_len = min(min_len, end-start+1)
+                curr_sum -= nums[start]
+                start += 1
+        return min_len if min_len != float("inf") else 0
+

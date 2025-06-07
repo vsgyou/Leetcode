3888f6a507cc5da559160288e829a57cdce1ab58
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-07 15:52:29 +0900

    Time: 28 ms (38.33%), Space: 12.6 MB (71.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,29 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        max_len = 0
-        for i in range(len(s)):
-            string_set = set()
-            for j in range(i, len(s)):
-                string_set.add(s[j])
-                if len(string_set) == j-i+1:
-                   max_len = max(max_len, j-i+1)
-                else:
-                    break
-
-        return max_len
-        
-        # 시간 오래걸림 -> Sliding Window
-        # start, end = 0, 0
         # max_len = 0
-        # s_set = set()
-        # while end < len(s):
-        #     if s[end] not in s_set:
-        #         s_set.add(s[end])
-        #         max_len = max(max_len, end-start+1)
-        #         end += 1
-        #     else:
-        #         s_set.remove(s[start])
-        #         start +=1
+        # for i in range(len(s)):
+        #     string_set = set()
+        #     for j in range(i, len(s)):
+        #         string_set.add(s[j])
+        #         if len(string_set) == j-i+1:
+        #            max_len = max(max_len, j-i+1)
+        #         else:
+        #             break
 
         # return max_len
+        
+        # 시간 오래걸림 -> Sliding Window
+        start, end = 0, 0
+        max_len = 0
+        s_set = set()
+        while end < len(s):
+            if s[end] not in s_set:
+                s_set.add(s[end])
+                max_len = max(max_len, end-start+1)
+                end += 1
+            else:
+                s_set.remove(s[start])
+                start +=1
+
+        return max_len

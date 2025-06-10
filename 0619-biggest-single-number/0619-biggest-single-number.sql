980ddaaa52a2776472c9ca6a4c2ca1693c8ec3c1
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-10 17:02:37 +0900

    Time: 386 ms (97.03%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT MAX(num) AS num
-FROM (
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-) AS SingleNumbers;
+select max(num) as num
+from (
+    select num
+    from MyNumbers
+    group by num
+    having count(num) = 1 ) as aa;

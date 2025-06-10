e5d4d3c2ba7218ef266e2fc3cecae0df476067dc
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-10 18:58:00 +0900

    Time: 858 ms (49.2%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,9 +1,4 @@
 # Write your MySQL query statement below
-DELETE P
-FROM Person P
-JOIN (
-    SELECT MIN(id) AS min_id, email
-    FROM Person
-    GROUP BY email
-) AS KeepRows
-ON P.email = KeepRows.email AND P.id > KeepRows.min_id;
+delete P
+from Person p, Person q
+where p.id > q.id and p.email = q.email

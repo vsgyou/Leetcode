6cf9f25f7bc27307018a98ec984ea7f9cc9fc11c
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-11 16:15:36 +0900

    Time: 376 ms (16.95%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -2,3 +2,8 @@
 select class
 from (select class, count(student) as stu from Courses group by class) as aa
 where stu >= 5
+
+-- select class
+-- from Courses
+-- group by class
+-- having count(student) >= 5;

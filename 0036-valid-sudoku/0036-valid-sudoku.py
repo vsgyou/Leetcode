20764cd2c8898f9cf6145eaf09fa94fd004d4e7f
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-14 01:20:32 +0900

    Time: 9 ms (44.95%), Space: 12.5 MB (16.11%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -5,31 +5,28 @@ class Solution(object):
         :rtype: bool
         """
         result = []
-        for row in (board):
+        for row in board:
             seen = set()
             for col in row:
-                if col != '.':
+                if col != ".":
                     if col in seen:
                         return False
                     seen.add(col)
-        
         for col in range(9):
             seen = set()
             for row in range(9):
-                if board[row][col] != '.':
+                if board[row][col] != ".":
                     if board[row][col] in seen:
                         return False
                     seen.add(board[row][col])
         
-        for mat_row in range(3):
-            for mat_col in range(3):
+        for rows in range(3):
+            for cols in range(3):
                 seen = set()
                 for row in range(3):
                     for col in range(3):
-                        r = mat_row*3 + row
-                        c = mat_col*3 + col
-                        if board[r][c] != '.':
-                            if board[r][c] in seen:
+                        if board[rows*3+row][cols*3+col] != ".":
+                            if board[rows*3+row][cols*3+col] in seen:
                                 return False
-                            seen.add(board[r][c])
+                            seen.add(board[rows*3+row][cols*3+col])
         return True

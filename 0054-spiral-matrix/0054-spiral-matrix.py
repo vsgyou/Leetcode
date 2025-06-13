05f50dc3113d0f5ce4b034d7ed2584de3429b2bb
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-14 02:10:04 +0900

    Time: 0 ms (100%), Space: 12.4 MB (52.82%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -5,17 +5,13 @@ class Solution(object):
         :rtype: List[int]
         """
         result = []
-
         while matrix:
             result += matrix.pop(0)
-
             if matrix and matrix[0]:
                 for row in matrix:
                     result.append(row.pop())
-            
             if matrix:
-                result+=matrix.pop()[::-1]
-            
+                result += matrix.pop()[::-1]
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
                     result.append(row.pop(0))

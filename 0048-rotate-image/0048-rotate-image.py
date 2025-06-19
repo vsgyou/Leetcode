57ce1fbcf6565a09e02a1ecf27771ba2b02a6d7b
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-06-19 23:42:16 +0900

    Time: 0 ms (100%), Space: 12.4 MB (86.69%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -8,8 +8,13 @@ class Solution(object):
         for i in range(n):
             for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
         for i in range(n):
             matrix[i].reverse()
         
         return matrix
+
+        """
+        [1, 2, 3] [1, 4, 7] [7, 4, 1]
+        [4, 5, 6] [2, 5, 8] [8, 5, 2]
+        [7, 8, 9] [3, 6, 9] [9, 6, 3]
+        """

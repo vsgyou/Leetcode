273a601a0b36d02809c9ba69d8edd2365c61acfe
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-03 21:29:46 +0900

    Time: 11 ms (51.6%), Space: 13.6 MB (90.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -8,5 +8,6 @@ class Solution(object):
         for num in nums:
             if count == 0:
                 candidate = num
-            count += (1 if num == candidate else -1)
+            count += (1 if candidate == num else -1)
+
         return candidate

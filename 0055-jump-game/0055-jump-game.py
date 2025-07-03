5b2c741d19927accb0a7a870efe82a9374c6939b
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-04 00:34:29 +0900

    Time: 46 ms (25.8%), Space: 13.3 MB (51.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,7 +7,7 @@ class Solution(object):
         max_jump = 0
         curr_idx = 0
         while curr_idx <= max_jump:
-            max_jump = max(max_jump, curr_idx+nums[curr_idx])
+            max_jump = max(curr_idx + nums[curr_idx], max_jump)
             if max_jump >= len(nums)-1:
                 return True
             curr_idx += 1

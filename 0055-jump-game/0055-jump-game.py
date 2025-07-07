95fa510b9967e79cf76d7e55fe08ca053d7bec6a
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-07 23:56:20 +0900

    Time: 45 ms (27.14%), Space: 13.3 MB (51.48%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,11 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        # max_jump = 0
+        # for i in range(len(nums)):
+        #     if i > max_jump:
+        #         return False
+        #     max_jump = max(i+nums[i], max_jump)
+        # return True
+
         max_jump = 0
         curr_idx = 0
-
-        for i in range(len(nums)):
-            if i > max_jump:
-                return False
-            max_jump = max(i+nums[i], max_jump)
-        return True
+        
+        while curr_idx <= max_jump:
+            max_jump = max(nums[curr_idx] + curr_idx, max_jump)
+            if max_jump >= len(nums)-1:
+                return True
+            curr_idx += 1
+        return False

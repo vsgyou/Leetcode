d1885d1bd94707783d3a589b77a7fd533a0abab2
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-08 00:39:54 +0900

    Time: 8 ms (63.67%), Space: 13.3 MB (47.84%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         curr_idx = 0
-        max_idx = 0
+        max_jump = 0
         jump_count = 0
 
         for i in range(len(nums)-1):
-            max_idx = max(i+nums[i], max_idx)
+            max_jump = max(i+nums[i], max_jump)
             if i == curr_idx:
-                curr_idx = max_idx
+                curr_idx = max_jump
                 jump_count += 1
             if curr_idx >= len(nums)-1:
                 break

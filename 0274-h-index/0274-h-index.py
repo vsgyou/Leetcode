6a33db1430ef4ab6ea0b986a34bc8ad9abe94996
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-09 20:13:47 +0900

    Time: 0 ms (100%), Space: 12.6 MB (86.7%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -3,6 +3,12 @@ class Solution(object):
         """
         :type citations: List[int]
         :rtype: int
+        [6, 5, 3, 1, 0]
+        0 -> 5
+        1 -> 4
+        2 -> 3
+        3 -> 2
+
         """
         citations.sort(reverse = True)
         num_cite = 0

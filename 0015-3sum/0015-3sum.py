aad26639e2eb747ebedf6c45a66bfc2598654125
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-15 22:11:48 +0900

    Time: 1908 ms (20.2%), Space: 20.5 MB (6.88%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,43 +3,21 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
+        [-4, -1, -1, 0, 1, 2]
         """
-        nums.sort()
+        nums = sorted(nums)
         result = set()
         for i in range(len(nums)-2):
-            left, right = i+1, len(nums)-1
-            while left < right:
-                hap = nums[i] + nums[left] + nums[right]
+            start, end = i+1, len(nums)-1
+            while start < end:
+                hap = nums[i] + nums[start] + nums[end]
                 if hap < 0:
-                    left += 1
+                    start += 1
                 elif hap > 0:
-                    right -= 1
+                    end -= 1
                 else:
-                    result.add((nums[i], nums[left], nums[right]))
-                    left += 1
-                    right -= 1
-        return [list(num) for num in result]
+                    result.add((nums[i], nums[start], nums[end]))
+                    start += 1
+                    end -= 1
 
-        # 이전코드 
-        # nums.sort()
-        # result = []
-        # for start in range(len(nums)-2):
-        #     if start >0 and nums[start] == nums[start-1]:
-        #         continue
-        #     left = start + 1
-        #     right = len(nums)-1
-        #     while left < right:
-        #         current_sum = nums[start] + nums[left] + nums[right]
-        #         if current_sum == 0 :
-        #             result.append([nums[start],nums[left],nums[right]])
-        #             while left < right and nums[left] == nums[left + 1]:
-        #                 left +=1 
-        #             while left < right and nums[right] == nums[right -1]:
-        #                 right -=1
-        #             left +=1 
-        #             right -= 1
-        #         elif current_sum < 0:
-        #             left += 1
-        #         else:
-        #             right -= 1
-        # return result
+        return [list(num) for num in result]

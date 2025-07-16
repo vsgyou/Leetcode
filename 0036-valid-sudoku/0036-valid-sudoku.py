8dcd618a05dbfa92c7c58e9963f9811763374ffe
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-17 01:16:26 +0900

    Time: 8 ms (51.97%), Space: 12.3 MB (85.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,29 +4,33 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-
-        for row in board:
+        for row in range(len(board)):
             seen = set()
-            for col in row:
-                if col != ".":
-                    if col in seen:
-                        return False
-                    seen.add(col)
-        for col in range(9):
-            seen = set()
-            for row in range(9):
+            for col in range(len(board[0])):
                 if board[row][col] != ".":
-                    if board[row][col] in seen:
+                    if board[row][col] not in seen:
+                        seen.add(board[row][col])
+                    else:
                         return False
-                    seen.add(board[row][col])
-        
-        for rows in range(3):
-            for cols in range(3):
+
+        for col in range(len(board)):
+            seen = set()
+            for row in range(len(board[0])):
+                if board[row][col] != ".":
+                    if board[row][col] not in seen:
+                        seen.add(board[row][col])
+                    else:
+                        return False
+
+        for i in range(3):
+            for j in range(3):
                 seen = set()
                 for row in range(3):
                     for col in range(3):
-                        if board[rows*3+row][cols*3+col] != ".":
-                            if board[rows*3+row][cols*3+col] in seen:
+                        if board[i*3+row][j*3+col] != ".":    
+                            if board[i*3+row][j*3+col] not in seen:
+                                seen.add(board[i*3+row][j*3+col])
+                            else:
                                 return False
-                            seen.add(board[rows*3+row][cols*3+col])
+            
         return True

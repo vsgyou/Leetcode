a4a593a5ea9b0e7e4a0ea032c0d27b249faf50f0
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-17 01:16:46 +0900

    Time: 11 ms (36.24%), Space: 12.4 MB (85.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -34,3 +34,29 @@ class Solution(object):
                                 return False
             
         return True
+
+        # for row in board:
+        #     seen = set()
+        #     for col in row:
+        #         if col != ".":
+        #             if col in seen:
+        #                 return False
+        #             seen.add(col)
+        # for col in range(9):
+        #     seen = set()
+        #     for row in range(9):
+        #         if board[row][col] != ".":
+        #             if board[row][col] in seen:
+        #                 return False
+        #             seen.add(board[row][col])
+        
+        # for rows in range(3):
+        #     for cols in range(3):
+        #         seen = set()
+        #         for row in range(3):
+        #             for col in range(3):
+        #                 if board[rows*3+row][cols*3+col] != ".":
+        #                     if board[rows*3+row][cols*3+col] in seen:
+        #                         return False
+        #                     seen.add(board[rows*3+row][cols*3+col])
+        # return True

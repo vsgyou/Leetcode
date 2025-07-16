3a208a1e407cffc46de0e88a52358b8964d1d4bf
vsgyou <105686490+vsgyou@users.noreply.github.com>
2025-07-17 01:43:46 +0900

    Time: 0 ms (100%), Space: 12.4 MB (86.91%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,18 +3,15 @@ class Solution(object):
         """
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
-        """
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i+1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for i in range(n):
-            matrix[i].reverse()
-        
-        return matrix
 
-        """
         [1, 2, 3] [1, 4, 7] [7, 4, 1]
         [4, 5, 6] [2, 5, 8] [8, 5, 2]
         [7, 8, 9] [3, 6, 9] [9, 6, 3]
         """
+
+        for i in range(len(matrix)):
+            for j in range(i+1,len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(len(matrix)):
+            matrix[i] = matrix[i][::-1]
